cfc33d9106d391bc9a4154d9b3ff614e129123b3
Anubhav Negi <anubhavnegi54@gmail.com>
2021-12-06 22:38:05 +0530

    Add files via upload

---
+++ Sorting Band Names without articles/index.js
@@ -0,0 +1,37 @@
+
+
+
+
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+
+function strip(bandName)
+{
+    return bandName.replace(/^(a |an |the )/i, '').trim();;
+}
+
+const sortedBands = bands.sort(function(a,b){
+
+    if ( strip(a) > strip(b))
+    {
+        return 1;
+    }
+
+    else{
+        return -1;
+    }
+})
+
+console.log(sortedBands);
+
+const bandsList = document.querySelector("#bands");
+
+
+const html = sortedBands.map( function (bands){
+    return `
+    <li>${bands}</li>
+
+    `;
+}).join('');
+
+bandsList.innerHTML = html;
